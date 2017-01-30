22d86fef249f52af7a7fd2d3e403780c309db505
sjshank <598524@CTS.COM>
2017-01-30 18:51:49 +0530

    Initial Commit

--- app/modules/post/postAdd.component.ts
+++ app/modules/post/postAdd.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from './post.service';
 
+import { IPost } from './post';
+
 @Component({
-    templateUrl:'app/modules/post/add.component.html'
+    templateUrl: 'app/modules/post/add.component.html'
 })
 
 export class AddPostComponent {
+    post: IPost;
+    postForm: FormGroup;
 
-    constructor(private _postService: PostService){}                
+    constructor(private _postService: PostService,
+        private _fb: FormBuilder) {
+
+            this.postForm = this._fb.group({
+                'name':['', Validators.required],
+                'userId': ['', Validators.required],
+                'title':['', [Validators.minLength(1), Validators.maxLength(20)]],
+                'body':['', [Validators.minLength(1), Validators.maxLength(360)]]
+            })
+    }
+
+
+    addPost() {
+
+    }
 }
